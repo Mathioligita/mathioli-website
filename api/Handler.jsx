4e94469869d4d4a77f0773afd05e7e4ca7920c6b
Ajin.T.S <149351487+Ajints@users.noreply.github.com>
2025-03-13 11:13:32 +0530

    Upddated correction for client meeting

--- api/Handler.jsx
+++ api/Handler.jsx
@@ -1,4 +1,3 @@
-
 // import { API_BASE_URL } from "@/app/utils";
 // import axios from "axios";
 // import Cookies from "js-cookie";
@@ -60,80 +59,85 @@
 
 // export default fetchHandler;
 
+import { API_BASE_URL } from '@/app/utils';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
+const fetchHandler = async ({
+	method,
+	endpoint,
+	data,
+	isFormData = false,
+	retries = 3,
+	backoff = 300,
+}) => {
+	console.log(API_BASE_URL, 'APIdata');
+	const API_BASE_URLS = `${API_BASE_URL}${endpoint}`;
+	const headers = {
+		'Content-Type': isFormData ? 'multipart/form-data' : 'application/json',
+	};
 
+	const accessToken = Cookies.get('accessToken');
 
-import { API_BASE_URL } from "@/app/utils";
-import axios from "axios";
-import Cookies from "js-cookie";
+	if (accessToken) {
+		headers['Authorization'] = `Bearer ${accessToken}`;
+	}
 
-const fetchHandler = async ({ method, endpoint, data, isFormData = false, retries = 3, backoff = 300 }) => {
-  const API_BASE_URLS = `${API_BASE_URL}${endpoint}`;
-  const headers = {
-    "Content-Type": isFormData ? "multipart/form-data" : "application/json",
-  };
+	const options = {
+		method,
+		url: API_BASE_URLS,
+		headers,
+		data: method !== 'GET' ? data : undefined,
+	};
 
-  const accessToken = Cookies.get("accessToken");
+	for (let i = 0; i < retries; i++) {
+		try {
+			const response = await axios.request(options);
+			return response?.data;
+		} catch (error) {
+			if (error.response) {
+				const { status, data, headers } = error.response;
 
-  if (accessToken) {
-    headers["Authorization"] = `Bearer ${accessToken}`;
-  }
+				if (status === 401) {
+					Cookies.remove('accessToken');
+					return {
+						isError: true,
+						data: 'Unauthorized access. Redirecting to login.',
+						status: 401,
+					};
+				}
 
-  const options = {
-    method,
-    url: API_BASE_URLS,
-    headers,
-    data: method !== "GET" ? data : undefined,
-  };
+				if (status === 404) {
+					return { isError: true, data: 'Not Found' };
+				}
 
-  for (let i = 0; i < retries; i++) {
-    try {
-      const response = await axios.request(options);
-      return response?.data;
-    } catch (error) {
-      if (error.response) {
-        const { status, data, headers } = error.response;
+				if (status === 429) {
+					const retryAfter = parseInt(headers['retry-after'], 10) || backoff * 2 ** i;
+					if (i < retries - 1) {
+						console.log(`Too Many Requests. Retrying in ${retryAfter}ms...`);
+						await new Promise((res) => setTimeout(res, retryAfter));
+						continue; // Retry the request
+					} else {
+						return {
+							isError: true,
+							data: 'Too Many Requests: Please try again later.',
+						};
+					}
+				}
 
-        if (status === 401) {
-          Cookies.remove("accessToken");
-          return { isError: true, data: "Unauthorized access. Redirecting to login.",status:401 };
-        }
-
-        if (status === 404) {
-          return { isError: true, data: "Not Found" };
-        }
-
-        if (status === 429) {
-          const retryAfter = parseInt(headers['retry-after'], 10) || backoff * (2 ** i);
-          if (i < retries - 1) {
-            console.log(`Too Many Requests. Retrying in ${retryAfter}ms...`);
-            await new Promise(res => setTimeout(res, retryAfter));
-            continue; // Retry the request
-          } else {
-            return { isError: true, data: "Too Many Requests: Please try again later." };
-          }
-        }
-
-        return { isError: true, data };
-      } else {
-        console.error("Network error:", error);
-        return { isError: true, data: "We can't process your request at this time. Please try later." };
-      }
-    }
-  }
+				return { isError: true, data };
+			} else {
+				console.error('Network error:', error);
+				return {
+					isError: true,
+					data: "We can't process your request at this time. Please try later.",
+				};
+			}
+		}
+	}
 };
 
 export default fetchHandler;
-
-
-
-
-
-
-
-
-
-
 
 // import { API_BASE_URL } from "@/app/utils";
 // import axios from "axios";
