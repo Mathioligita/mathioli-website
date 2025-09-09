9cc77430d1a23b6eecb505d071ddf2b9d72f36b5
Ajin.T.S <149351487+Ajints@users.noreply.github.com>
2025-09-09 16:15:41 +0530

    robo text added and site map added

--- src/app/404/page.jsx
+++ src/app/404/page.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const NotFoundPage = () => {
   return (
     <div>
+      {/* Not Found */}
       <h1>404 Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <p>The page you are looking for does not exist</p>
     </div>
   );
 };
